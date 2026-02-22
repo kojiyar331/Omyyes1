bf8f0885a25c329a493a916fa41c87ec3a86c7dc
Koji yar <kojiyar331@gmail.com>
2026-02-22 13:16:00 +0000

    Upload New File

---
+++ models/commands/INFO.js
@@ -0,0 +1,32 @@
+module.exports.config = {
+  name: "inf",
+  version: "1.0.1", 
+  hasPermssion: 0,
+  credits:"ARIF BABU" ,
+  description: "THIS BOT IS MADE BY ARIF BABU",
+  usePrefix: true,
+  commandCategory: "INFORMATION",
+  cooldowns: 1,
+  dependencies: 
+  {
+    "request":"",
+    "fs-extra":"",
+    "axios":""
+  }
+};
+module.exports.run = async function({ api,event,args,client,Users,Threads,__GLOBAL,Currencies }) {
+const axios = global.nodemodule["axios"];
+const request = global.nodemodule["request"];
+const fs = global.nodemodule["fs-extra"];
+const time = process.uptime(),
+    hours = Math.floor(time / (60 * 60)),
+    minutes = Math.floor((time % (60 * 60)) / 60),
+    seconds = Math.floor(time % 60);
+const moment = require("moment-timezone");
+var juswa = moment.tz("Asia/Karachi").format("DD/MM/YYYY");
+var link =                                     
+["https://i.imgur.com/1tUVG85.jpeg"];
+var callback = () => api.sendMessage({body:`┏━━━━━┓\n     TAHA-KHAN-BABU                    ✧═══•❁🙊❁•═══✧\n┗━━━━━┛\n\n\n         ❁ ═════════ ❃•❃ ═════════ ❁\n                            ♥️ 𝗜𝗡𝗙𝗢𝗥𝗠𝗔𝗧𝗜𝗢𝗡 ♥️\n         ❁ ═════════ ❃•❃ ═════════ ❁\n\n\n\n🌺  [ 1 ]  𒁍 BOT NAME   →   ${global.config.BOTNAME}\n🌺  [ 2 ]  𒁍 BOT ADMIN   →   TAHA-BABU\n🌺  [ 3 ]  𒁍 BOT  PREFIX   →   [ ${global.config.PREFIX} ]\n🌺  [ 4 ]  𒁍  DATE   → ${juswa}\n🌺  [ 5 ]  𒁍  BOT RUNNING TIME  →  [ ${hours}:${minutes}:${seconds} ]\n🌺  [ 6 ]  𒁍  https://www.facebook.com/profile.php?id=100085636015827\n🌺  [ 7 ]  𒁍 WHATSAPP → [ +93 3474771404 ]\n🌺  [ 8 ]  𒁍 INSTAGRAM →  https://www\n🌺  [ 9 ]  𒁍 YOUTUBE  →  https://www.youtube.com/@tahachandia\n🌺  [ 10 ]  𒁍 GITHUB  →  NA\n🌺THANKYOU FOR USING ${global.config.BOTNAME} BOT♥️\n\n\n
+`,attachment: fs.createReadStream(__dirname + "/cache/juswa.jpg")}, event.threadID, () => fs.unlinkSync(__dirname + "/cache/juswa.jpg")); 
+      return request(encodeURI(link[Math.floor(Math.random() * link.length)])).pipe(fs.createWriteStream(__dirname+"/cache/juswa.jpg")).on("close",() => callback());
+   };
